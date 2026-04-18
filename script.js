abe4ba7a93c7ab131c55fbc59f83b03ac7894088
eian <shoguns.reefs.0i@icloud.com>
2026-04-18 14:24:10 +0800

    Initialize Supabase client with URL and key
    Added Supabase URL and key for client initialization.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // script.js
 // 1. Supabase 設定
+const SUPABASE_URL = "https://krfltdgjrymcyacolvmw.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtyZmx0ZGdqcnltY3lhY29sdm13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0NTM5MTQsImV4cCI6MjA5MjAyOTkxNH0.uCBvWr2Wl600iKsu6cuF5-4t3SSxF1KaNP5NacfzSgw";
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // 2. 登入監聽與權限切換
