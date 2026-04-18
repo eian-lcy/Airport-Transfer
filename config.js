2acca55017e70fcfbb9d3622c8092af193d92e52
eian <shoguns.reefs.0i@icloud.com>
2026-04-18 14:22:34 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,2 @@
-const CONFIG = {
-    SUPABASE_URL: 'https://krfltdgjrymcyacolvmw.supabase.co',
-    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtyZmx0ZGdqcnltY3lhY29sdm13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0NTM5MTQsImV4cCI6MjA5MjAyOTkxNH0.uCBvWr2Wl600iKsu6cuF5-4t3SSxF1KaNP5NacfzSgw'
-};
+const SUPABASE_URL = "https://krfltdgjrymcyacolvmw.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtyZmx0ZGdqcnltY3lhY29sdm13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0NTM5MTQsImV4cCI6MjA5MjAyOTkxNH0.uCBvWr2Wl600iKsu6cuF5-4t3SSxF1KaNP5NacfzSgw";
