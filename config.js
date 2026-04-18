ac64450b39bb01064affadeac72d364194db0209
eian <shoguns.reefs.0i@icloud.com>
2026-04-18 15:48:42 +0800

    Delete config.js

--- config.js
+++
@@ -1,2 +0,0 @@
-const SUPABASE_URL = "https://krfltdgjrymcyacolvmw.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtyZmx0ZGdqcnltY3lhY29sdm13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0NTM5MTQsImV4cCI6MjA5MjAyOTkxNH0.uCBvWr2Wl600iKsu6cuF5-4t3SSxF1KaNP5NacfzSgw";
