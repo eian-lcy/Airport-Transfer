6ba349d0960e25d694182677af6617b3ffc9ad09
eian <shoguns.reefs.0i@icloud.com>
2026-04-18 19:15:45 +0800

    Add Supabase configuration constants

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // script.js
+const SUPABASE_URL = "https://krfltdgjrymcyacolvmw.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtyZmx0ZGdqcnltY3lhY29sdm13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0NTM5MTQsImV4cCI6MjA5MjAyOTkxNH0.uCBvWr2Wl600iKsu6cuF5-4t3SSxF1KaNP5NacfzSgw";
 // 1. Supabase 設定
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
